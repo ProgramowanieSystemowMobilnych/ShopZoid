d39e6f6d7555f47cd9aaf0e8eb611ac22d76afd7
Trzmiel1 <48628363+Trzmiel1@users.noreply.github.com>
2020-05-14 13:14:19 +0200

    add main.js

---
+++ js/main.js
@@ -0,0 +1,12 @@
+if ('serviceWorker' in navigator) {
+
+    navigator.serviceWorker
+        .register('./serviceWorker.js', { scope: './' })
+        .then(function(registration) {
+            console.log("Service Worker Registered. Scope is:"+registration.scope);
+        })
+        .catch(function(err) {
+            console.log("Service Worker Failed to Register", err);
+        })
+
+}
